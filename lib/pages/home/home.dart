5e290c4f4bc899cdccd0b09635c9159a2f2361a9
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-06-23 16:43:31 +0300

    toggle favorite on double tap

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,161 +1,61 @@
 import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:poetry_app/components/poem_card.dart';
 import 'package:poetry_app/models/poem/poem.dart';
-import 'package:poetry_app/pages/settings/settings.dart';
-import 'package:poetry_app/services/interfaces/poem_repository_interface.dart';
-import 'dart:async';
-import 'dart:math' as math;
+import 'package:poetry_app/providers/favorite_provider.dart';
+import 'package:poetry_app/providers/poem_provider.dart';
+
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
-  Home({super.key});
+  const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
-  static const int IDS_PER_BATCH = 10;
-  static const int CARDS_ON_SCREEN = 2;
-  final PoemRepositoryI _poemRepository = GetIt.I.get<PoemRepositoryI>();
-  final StreamController<List<int>> _poemIdStream =
-      StreamController<List<int>>();
-
-  // store only visibile poems (dont load all)
-  final List<Poem?> _activePoems =
-      List<Poem?>.generate(CARDS_ON_SCREEN, (index) => null);
-
-  // store colors for visible poems
-  final List<Color> _activePoemsColors =
-      List<Color>.generate(CARDS_ON_SCREEN, (index) => Colors.white);
-
-  // actual ids for poems (load as user swipes)
-  final List<int> _poemIds = [];
-  // to make sure all poems are marked read before getting another batch
-  List<Future<void>> _poemMarkReadFutures = [];
-
-  // initially load poems
-  Future<List<int>> _initPoems() async {
-    List<int> poemIds =
-        await _poemRepository.getOldestAll(limit: IDS_PER_BATCH);
-    _poemIds.addAll(poemIds);
-    return poemIds;
-  }
+class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin {
+  final _keepAlive = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Poems'),
-        actions: [_settingsButton(context)],
-      ),
-      body: FutureBuilder(
-          future: _initPoems(),
-          builder: (_, snapshot) {
-            if (snapshot.hasData) {
-              return StreamBuilder(
-                stream: _poemIdStream.stream,
-                initialData: snapshot.data!,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    _poemIds.addAll(snapshot.data!);
-                    return _poemCards();
-                  } else if (snapshot.hasError) {
-                    return const Center(child: Text('error'));
-                  } else {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                },
+    super.build(context);
+
+    return Consumer<PoemProvider>(
+      builder: (context, provider, child) {
+        return provider.isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Center(
+                child: FractionallySizedBox(
+                  heightFactor: 0.8,
+                  widthFactor: 0.95,
+                  child: AppinioSwiper(
+                    cardsCount: provider.poems.length,
+                    onSwipe: (index, direction) {
+                      provider.markRead(index - 1);
+                    },
+                    onEnd: provider.loadPoemBatch,
+                    cardsBuilder: (_, int index) {
+                      Poem? poem = provider.getPoem(index);
+                      return Consumer<FavoriteProvider>(
+                          builder: (context, favorites, child) {
+                        return PoemCard(
+                            onDoubleTap: () async {
+                              if (favorites.is_loading) return;
+                              await favorites.favoritePoem(poem);
+                              provider.refreshPoem(poem.id);
+                            },
+                            poem: poem,
+                            color: provider.getColor(index));
+                      });
+                    },
+                  ),
+                ),
               );
-            }
-            if (snapshot.hasError) {
-              return const Center(child: Text('Error'));
-            }
-            return const Center(child: CircularProgressIndicator());
-          }),
+      },
     );
   }
 
   @override
-  void dispose() {
-    _poemIdStream.close();
-    super.dispose();
-  }
-
-  Widget _poemCards() {
-    return Center(
-      child: FractionallySizedBox(
-        heightFactor: 0.75,
-        widthFactor: 0.95,
-        child: AppinioSwiper(
-          cardsCount: _poemIds.length,
-          onSwipe: (index, direction) {
-            final int lastIndex = (index - 1) % CARDS_ON_SCREEN;
-            // mark as read the previous poem
-            if (_activePoems[lastIndex] != null) {
-              _poemMarkReadFutures
-                  .add(_poemRepository.markRead(_activePoems[lastIndex]!));
-            }
-            // mark previous card as unloaded
-            _activePoems[lastIndex] = null;
-          },
-          onEnd: _loadPoemBatch,
-          cardsBuilder: (_, int index) {
-            if (_activePoems[index % CARDS_ON_SCREEN] != null) {
-              return PoemCard(
-                poem: _activePoems[index % CARDS_ON_SCREEN]!,
-                color: _activePoemsColors[index % CARDS_ON_SCREEN],
-              );
-            }
-            return FutureBuilder(
-              future: _loadPoem(index),
-              builder: (_, AsyncSnapshot<Poem> snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return PoemCard(
-                    poem: snapshot.data!,
-                    color: _activePoemsColors[index % CARDS_ON_SCREEN],
-                  );
-                }
-                return const Center(child: CircularProgressIndicator());
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  // load a poem from database at using saved id at position index
-  Future<Poem> _loadPoem(int index) async {
-    Poem poem = (await _poemRepository.findById(_poemIds[index]))!;
-    // put in the cached list
-    _activePoems[index % CARDS_ON_SCREEN] = poem;
-    // generate a random color
-    _activePoemsColors[index % CARDS_ON_SCREEN] = Colors
-        .primaries[math.Random().nextInt(Colors.primaries.length)].shade50;
-    return poem;
-  }
-
-  // load a batch of unread poem ids from database
-  Future<void> _loadPoemBatch() async {
-    // wait for marked poems
-    await Future.wait(_poemMarkReadFutures);
-    List<int> poemIds =
-        await _poemRepository.getOldestAll(limit: IDS_PER_BATCH);
-    _poemIdStream.add(poemIds);
-    // reset marked poems list
-    _poemMarkReadFutures = [];
-  }
-
-  Widget _settingsButton(BuildContext context) {
-    return IconButton(
-      onPressed: () => Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (c) => const Settings(),
-        ),
-      ),
-      icon: const Icon(Icons.settings),
-    );
-  }
+  bool get wantKeepAlive => _keepAlive;
 }
