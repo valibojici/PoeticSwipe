8ccc34d1094a7b20ebc855bd9250127daedbafc2
Valentin Bojici <68808448+valibojici@users.noreply.github.com>
2023-06-23 16:42:31 +0300

    change UI

--- lib/components/poem_card.dart
+++ lib/components/poem_card.dart
@@ -5,12 +5,15 @@ import 'package:poetry_app/models/poem/poem.dart';
 import 'package:poetry_app/pages/poem/poem.dart';
 
 class PoemCard extends StatelessWidget {
-  final Poem poem;
+  final Poem? poem;
   final Color color;
+  final Function()? onDoubleTap;
+
   const PoemCard(
       {super.key,
       required this.poem,
-      this.color = const Color.fromRGBO(255, 224, 178, 1)});
+      this.color = const Color.fromRGBO(255, 224, 178, 1),
+      this.onDoubleTap});
   final TextStyle titleStyle =
       const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
 
@@ -19,32 +22,47 @@ class PoemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.of(context)
-          .push(MaterialPageRoute(builder: (_) => PoemView(poem: poem))),
+      onTap: poem == null
+          ? null
+          : () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => PoemView(poem: poem!))),
+      onDoubleTap: onDoubleTap,
       child: Card(
         elevation: 16,
         color: color,
         // color: Colors.orange[100],
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(poem.title, style: titleStyle),
-              Text(poem.author, style: authorStyle),
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Text(
-                    poem.poem,
-                    overflow: TextOverflow.fade,
-                  ),
+          child: poem == null
+              ? Center(child: CircularProgressIndicator())
+              : Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: _getIcon(),
+                    ),
+                    Text(poem!.title, style: titleStyle),
+                    Text(poem!.author, style: authorStyle),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                          poem!.poem,
+                          overflow: TextOverflow.fade,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
         ),
       ),
     );
+  }
+
+  Widget _getIcon() {
+    return Icon(
+        poem!.favoriteTime == null ? Icons.favorite_border : Icons.favorite);
   }
 }
